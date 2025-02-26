340159f8c8132bde63bebb8b913d61c9c8f693ac
Erwin van Eijndhoven <erwin.anita@kpnmail.nl>
2025-02-26 19:10:40 +0100

    bestand tijdelijk.py gecodeerd

--- tijdelijk.py
+++ tijdelijk.py
@@ -8,10 +8,10 @@ reclame_tekst = "vandaag is in de aanbieding: vanille-ijs, 1 liter - slechts eur
 reclame_tekst3 = (reclame_tekst.swapcase())
 reclame_tekst4 = ["VANDAAG", "IN", "DE", "AANBIEDING", ":", "VANILLE-IJS", "1", "LITER", "-", "SLECHTS", "EURO", "3.20"]
 for e1 in reclame_tekst4:
-if len e1 >=5:
-    print(e1.upper())
-else:
-    print(e1.lower())
+    if len (e1) >=5:
+        print(e1.upper())
+    else:
+        print(e1.lower())
 
 
 
